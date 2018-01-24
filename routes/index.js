57561e99e53e4696dc25f53c32c435e59bfd8d9b
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-01-24 11:30:09 +0700

    Add get, post, put, delete from express for API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,108 @@
 var express = require('express');
 var router = express.Router();
 
+let data = [{
+  id: 0,
+  name: `Steve Jobs`,
+  city: `San Francisco`
+}, {
+  id: 1,
+  name: `Linus Torvalds`,
+  city: `New York`
+}, {
+  id: 2,
+  name: `Bill Gates`,
+  city: `Sillicon Valley`
+}];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+// GET all items
+router.get(`/api/items`, (req, res) => {
+  res.send({
+    message: `Here's your data`,
+    data: data
+  });
+});
+
+// GET single item
+router.get(`/api/items/:id`, (req, res) => {
+  const items = data;
+  const itemId = Number(req.params.id);
+  const item = items.filter(item => {
+    return item.id === itemId;
+  });
+  res.send({
+    message: `Get data through by id`,
+    data: item
+  });
+})
+
+// POST an item
+router.post(`/api/items`, (req, res) => {
+  const item = {
+    id: data.length,
+    name: req.body.name,
+    city: req.body.city
+  };
+  const items = data.push(item);
+  res.send({
+    message: `New data was added`,
+    data: data
+  });
+});
+
+// DELETE an item
+router.delete(`/api/items/:id`, (req, res) => {
+  let items = data;
+  let itemId = Number(req.params.id);
+  let currentData = items.filter(item => {
+    return item.id !== itemId;
+  });
+  data = currentData;
+  res.send({
+    message: `An item was deleted`,
+    data: data
+  });
+});
+
+// DELETE all items
+router.delete(`/api/items`, (req, res) => {
+  data.splice(0, data.length);
+  res.send({
+    message: `All items was deleted`,
+    data: data
+  });
+});
+
+// UPDATE an item
+router.put(`/api/items/:id`, (req, res) => {
+  let id = Number(req.params.id);
+  let name = req.body.name;
+  let city = req.body.city;
+
+  // Find data index
+  let itemIndex = data.findIndex((item, index) => {
+    return item.id === id;
+  });
+
+  // Matching matched data
+  data[itemIndex][`name`] = name;
+  data[itemIndex][`city`] = city;
+
+  // Prepare response
+  let response = {
+    message: `Data was updated through by id`,
+    data: data
+  };
+
+  // Send response
+  res.send(response);
+
 });
 
 module.exports = router;
